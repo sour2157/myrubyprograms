476ee77a9f1af8ac302562843e55d49ba22a290a
sourabh raje <sour2157@gmail.com>
2015-09-09 17:24:31 +0530

    answer

---
+++ 1_week/2e_sprintf.rb
@@ -0,0 +1,14 @@
+#Read the sprintf documentation and the % documentation in the String class and figure out the output being printed by of this Ruby code.
+
+#puts "%05d" % 123
+
+
+
+
+sprintf format_string
+
+%[flags][width][.precision]type
+
+For %05d:
+
+flags: 0 = Pad with zeros, not spaces. width: 5 type: d = Convert argument as a decimal number.
