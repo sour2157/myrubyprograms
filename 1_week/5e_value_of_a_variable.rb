dfa4de03b36c10843a35941b50b08cb9cf41eafd
sourabh raje <sour2157@gmail.com>
2015-09-09 17:15:56 +0530

    Answer

---
+++ 1_week/5e_value_of_a_variable.rb
@@ -0,0 +1,15 @@
+#Exercise 5. The following program prints the value of the variable. Why?
+
+#my_string = 'Hello Ruby World'
+#def my_string
+#  'Hello World'
+#end
+#puts my_string
+
+
+
+
+
+
+In the program the puts command is pointing to the variable name so its prints the value of the variable.
+The method is never called so it never goes in there..
